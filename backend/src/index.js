d1543c0469211a6d14fc3ceb0e4b10aeea1972a1
rosjerry <rostomi1306@gmail.com>
2025-08-08 12:40:45 +0400

    feat: implement all link logic and trycatch

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,73 +1,71 @@
-const port = 3000
-import { nanoid } from 'nanoid'
+import { nanoid } from "nanoid";
+import { createClient } from "redis";
+import express from "express";
 
-import express from 'express'
-const app = express()
+const port = 3000;
+const app = express();
+const client = createClient();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-import { createClient } from 'redis';
-const client = createClient();
-
-client.on('error', err => console.log('Redis Client Error', err));
+client.on("error", (err) => console.log("Redis Client Error", err));
 
 await client.connect();
 
-let dummyData=[
-  {
-    "a1b2c3": "https://google.com"
-  },
-  {
-    "a1b2c4": "https://youtube.com"
-  },
-  // {
-  //   "a1b2c5": "https://facebook.com"
-  // },
-  // {
-  //   "a1b2c6": "https://apple.com"
-  // },
-  // {
-  //   "a1b2c7": "https://amazon.com"
-  // },
-]
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.post('/create', async (req, res) => {
-
-  const longURL = req?.body?.longURL;
-  const shortURL = nanoid(7);
-  const result =  await client.set(shortURL, longURL);
-  const savedValue = await client.get(shortURL);
-
-  res.send({
-    message: "URL Shortened!",
-    result: result,
-    shortURL: shortURL,
-    longURL: savedValue
-  })
-})
-
-app.get('/:shortURL', (req, res) => {
-  const shortURL = req.params.shortURL;
-
-  dummyData.filter((url) => {
-    if (Object.keys(url) != shortURL){
-      console.log("could not found")
-    }else {
-      console.log(shortURL)
-      console.log(Object.keys(url) == shortURL)
-      console.log(Object.values(url))
-      res.send(...(Object.values(url)))
+app.get("/", async (req, res) => {
+  try {
+    const keys = await client.keys("*");
+    if (keys.length === 0) {
+      return res.json({});
     }
-  })
 
-  res.send("1")
-})
+    const values = await client.mGet(keys);
+    const data = {};
+    keys.forEach((key, index) => {
+      data[key] = values[index];
+    });
+
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.post("/create", async (req, res) => {
+  try {
+    const longURL = req?.body?.longURL;
+    const shortURL = nanoid(7);
+    const result = await client.set(shortURL, longURL);
+    const savedValue = await client.get(shortURL);
+
+    res.send({
+      message: "URL Shortened!",
+      result: result,
+      shortURL: shortURL,
+      longURL: savedValue,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.get("/:shortURL", async (req, res) => {
+  try {
+    const shortURL = req.params.shortURL;
+    const retrievedValue = await client.get(shortURL);
+    if (!retrievedValue) {
+      return res.status(404).json({ error: "Short URL not found" });
+    }
+    res.redirect(retrievedValue);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
